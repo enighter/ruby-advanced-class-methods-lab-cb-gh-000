29b1afb239ada63cb7bbf349e51c5e42a82dea39
Jade Anderson <enighter@gmail.com>
2017-07-15 16:34:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -25,12 +25,9 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.each do |song|
-      if song.name == name
-        return song
-      end
+    self.all.find do |song|
+      song.name == name
     end
-    return nil
   end
 
   def self.find_or_create_by_name(name)
