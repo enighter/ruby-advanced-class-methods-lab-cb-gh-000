4861213921cad9b300b13b24179eebeac2e6b949
Jade Anderson <enighter@gmail.com>
2017-07-15 16:29:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,61 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.each do |song|
+      if song.name == name
+        return song
+      end
+    end
+    return nil
+  end
+
+  def self.find_or_create_by_name(name)
+    existing = self.find_by_name(name)
+    existing == nil ? self.create_by_name(name) : existing
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    filename_array = filename.split(" - ")
+    name = filename_array[1].split(".")[0]
+    artist_name = filename_array[0]
+    song = self.new_by_name(name)
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
   def save
     self.class.all << self
   end
